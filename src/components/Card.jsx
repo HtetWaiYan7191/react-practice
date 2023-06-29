fe070ef059a6562e7d9e557ad0f4603af8bfa812
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-29 20:26:21 +0630

    Test the useState hook

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import { useState } from 'react'
 function Card({product}) {
   const [title, setTitle] = useState(product.title);
+  const [showPrice, setShowPrice] = useState('Hide Price');
+  const [productPrice, setProductPrice] = useState(product.price);
+
+  const hidePrice = () => {
+    if(showPrice === 'Show Price') {
+      setShowPrice('Hide Price');
+      setProductPrice(product.price)
+    }
+    else {
+      setShowPrice('Show Price')
+      setProductPrice('')
+    }
+  };
+
   return (
     <div className="card" style={{width: "18rem"}}>
     <img src={product.image} className="card-img-top" alt="..."></img>
     <div className="card-body">
       <h5 className="card-title text-truncate ">{title}</h5>
       <p className="card-text">{product.description}</p>
-      <a href="#" className="btn btn-primary" onClick={() => {setTitle('new Title')}}>Go somewhere</a>
+      <div className="d-flex justify-content-between">
+      <a href="#" className="btn btn-primary" onClick={hidePrice} >{showPrice}</a>
+      <span className='btn text-end'>{productPrice}</span>
+      </div>
+      
     </div>
   </div>
   )
