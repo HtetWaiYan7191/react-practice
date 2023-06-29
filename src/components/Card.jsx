71ef642a449f8b9e7f8e3a60afbaf419c3b1bdf3
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-29 19:56:33 +0630

    practice useState hook

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import { useState } from 'react'
 function Card({product}) {
+  const [title, setTitle] = useState(product.title);
   return (
     <div className="card" style={{width: "18rem"}}>
     <img src={product.image} className="card-img-top" alt="..."></img>
     <div className="card-body">
-      <h5 className="card-title text-truncate ">{product.title}</h5>
+      <h5 className="card-title text-truncate ">{title}</h5>
       <p className="card-text">{product.description}</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
+      <a href="#" className="btn btn-primary" onClick={() => {setTitle('new Title')}}>Go somewhere</a>
     </div>
   </div>
   )
