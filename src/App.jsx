bca8a115e0c181cc394260bf3447067eee3cf347
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-29 12:11:07 +0630

    Get data from fakeapi and show them as cards in browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import Home from "./Home";
+import Product from "./components/Product";
 import "./App.css";
 
 const App = () => {
-  const name = "Htet Wai Yan";
+  
   return (
     <>
-      <h1>Welcome To My React App</h1>
-      <Home userName = {name}></Home>
+      <h1 className="main-title">Welcome To My React App</h1>
+      <Product></Product>
       </>
   );
 };
