ce09ac77680a0a15aa0974aa21b9ac238d0cf452
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-30 21:47:29 +0630

    Add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Contact = () => {
   return (
     <div>
+      <Navbar></Navbar>
       This is Contact
     </div>
   )
