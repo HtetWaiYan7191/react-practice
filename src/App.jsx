afb1c651426aa01258bd5cdc82bd17cb649df87c
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-30 21:17:53 +0630

    Use react router dom for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import Product from "./components/Product";
+import ReactDOM from "react-dom";;
 import "./App.css";
-import Navbar from "./components/Navbar";
-import AddProductForm from "./components/AddProductForm";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -20,12 +21,11 @@ const App = () => {
 
   return (
     <>
-    <Navbar></Navbar>
-      <h1 className="main-title my-5">Welcome To My React App</h1>
-      <div className="card-container">
-      <AddProductForm products={products} setProducts={setProducts}></AddProductForm>
-      <Product products={products}></Product>
-      </div>
+    <Routes>
+    <Route path='/' element ={<Home products={products} setProducts={setProducts}/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    <Route path="/contact" element={<Contact/>}></Route>
+    </Routes>
 
       </>
   );
