f944ae2dc5aa0b8d47d07b387457433d1c8484f7
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-30 21:47:16 +0630

    Update about.jsx component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 const About = () => {
   return (
     <div>
+      <Navbar></Navbar>
       This is about
     </div>
   )
