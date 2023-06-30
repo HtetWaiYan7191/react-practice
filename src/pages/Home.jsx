6c62b05d9da84e9a326f6e1ddc5fc96fe89a5593
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-30 21:17:17 +0630

    Create page folder for different pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import AddProductForm from '../components/AddProductForm';
+import Product from '../components/Product';
+
+
+function Home({products, setProducts}) {
+  return (
+    <>
+    <Navbar></Navbar>
+    <h1 className="main-title my-5">Welcome To My React App</h1>
+    <div className="card-container">
+    <AddProductForm products={products} setProducts={setProducts}></AddProductForm>
+    <Product products={products}></Product>
+    </div>
+    </>
+    
+  )
+}
+
+export default Home
